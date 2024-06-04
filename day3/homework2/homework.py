69d0aced99b9378076d99386b07da345ee89cf8d
ilialobjanidze <lobjanidzeilia10@gmail.com>
2024-06-04 19:51:08 +0400

    second commit

---
+++ day3/homework2/homework.py
@@ -0,0 +1,10 @@
+num1 = 5
+num2 = 25
+num3= 42
+
+example1 = num1 + num2
+example2 = num2 / num1 
+example3 = num3 - num1
+example4 = int(num2 / num1)
+example5 = float (num2/ num1)
+print(example5)
