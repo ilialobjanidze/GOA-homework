a898d364cfbfaec15e35ee6a1e2b08159764cd28
ilialobjanidze <lobjanidzeilia10@gmail.com>
2024-06-04 19:44:03 +0400

    second commit

---
+++ day 1/homework1/homework.py
@@ -0,0 +1,9 @@
+dog_name1 = "Rick"
+dog_name2 = "John"
+dog_name3 = "Max"
+
+#We gave each variable its value 
+
+#now lets use variables to print first and second dog's name
+
+print(f"first dog's name is {dog_name1} and segond dog's name is {dog_name2}")
---
+++ day 1/homework1/homework2.py
@@ -0,0 +1,36 @@
+#i used 4 variables and gave all of them theyr values
+name = "Ilia"
+last_name = "lobjanidze"
+age = "14"
+birthmonth = "february"
+example1 = (f"My name is {name} {last_name} , {age} years old. and my birthmonth is {birthmonth}")
+
+product = "smartphone"
+brand = "Apple"
+price = "3500 GEL"
+example2 = (f"The {brand} {product} costs {price} ")
+
+#i used different variables this time to tell the customer price of the smartphone
+
+team1 = "Georgia"
+team2 = "Portugal"
+score_team1 = "2"
+score_team2 = "1" 
+goalscorer1 = "budu zivzivadze"
+goalscorer2 = "otar kiteishvili"
+
+excample3 =  (f"{team1} is beating {team2} and the score is {team1} {score_team1} and {team2} {score_team2}, goalscorers for {team1} were {goalscorer1} and {goalscorer2} ")
+
+
+car = "mercedes"
+price = "$60,000K "
+
+example4 = (f"wow, what a great car. whats a name and price again? its name is {car} and price is {price} ")
+
+
+age = "20 years old"
+name = "ana"
+job = "web developer"
+experience = "4 years"
+
+print(f"hello, im {name} i am  {age} i am working as a {job} and i have been working for over {experience}")
