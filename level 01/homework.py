1e752bacf55f7514ff6c7d718b62aaebbf1644ec
ilialobjanidze <lobjanidzeilia10@gmail.com>
2024-06-11 14:50:32 +0400

    fifth commit

---
+++ level 01/homework.py
@@ -0,0 +1,9 @@
+name = "Ilia"
+last_name = "Lobjanidze"
+Age = 14
+studies_at = "Goal Oriented Academy"
+Nationality = "Georgian"
+Birth_Place = "Tbilisi"
+Height = 1.78
+
+print(f"My name is {name} and last name is {last_name}. I am {Age} years old, and I study at {studies_at}. I am from {Nationality}. I was born in {Birth_Place}, and for now, my height is {Height}. That's all, goodbye.")
