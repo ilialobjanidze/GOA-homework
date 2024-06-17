11362c1f14e652506200208ec636ccdc4f5454fb
ilialobjanidze <lobjanidzeilia10@gmail.com>
2024-06-17 13:15:19 +0400

    sixth commit

---
+++ level 08/homework.py
@@ -0,0 +1,25 @@
+num1 = 10
+num2 = 5
+price = 30
+ticket = 10
+
+# print(num1 > num2)
+# print(num1 >= num2)
+# print(True or True or False)
+# print(num1 <= num2)
+# print(num1 = price)
+# print(False or False or False)
+# print(price <= num2)
+print(True or False or False or False)
+# print(ticket != num2)
+# print(num1 != ticket)
+# print(num1 == ticket)
+# print(True and True and True and False and True) 
+# print(num2 <! ticket)
+# print(ticket == num2)
+# print(num2 != price)
+# print(True and True and True and True) true
+# print(price < ticket)
+# print(num1 > num2)
+# prunt(ticket == price)
+# print(ticket != price)
