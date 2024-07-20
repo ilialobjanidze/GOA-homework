d93cd2582c4ce5d79259be22f69d7a161964c62a
ilialobjanidze <lobjanidzeilia10@gmail.com>
2024-07-20 19:51:21 +0400

    eleventh commit

---
+++ level 18/homework1.py
@@ -0,0 +1,65 @@
+def sum_of_two_numbers(a, b):
+    return a + b
+result = sum_of_two_numbers(3, 5)
+print(result)
+
+
+
+
+def three_numbers_multiplie(a, b, c):
+    return a + b
+result2 = three_numbers_multiplie(9, 15, 42 )
+print(result2)  
+
+
+def greet():
+    name = input("Please Enter Your Name")
+    last_name = input("Please Enter Your last Name")
+
+    print(f"Welcome back {name} {last_name}. Have a Nice Day")
+
+def sum_and_assign(a, b):
+    sum_result = a + b
+    return sum_result
+
+num1 = 5
+num2 = 3
+result = sum_and_assign(num1,num2)
+print(f"The sum of {num1} and {num2} is: {result}")  
+
+
+
+def multiply_and_assign(a, b, c):
+    product = a * b * c
+    return product
+
+x = 32
+y = 132
+z = 16
+result = multiply_and_assign(x, y, z)
+print(f"The product of {x}, {y}, and {z} is: {result}")
+
+def print_foods(foods):
+    for food in foods:
+        print(food)
+
+# მაგალითი:
+foods_list = ["khinkali", "Pasta", "Pizza", "Lobio"]
+print_foods(foods_list)
+
+
+
+def print_numbers(numbers):
+    for number in numbers:
+        print(number)
+
+
+num_list = [14, 923 ,141 ,93, 12, 83, 63, 32]
+print_numbers(num_list)
+
+
+
+
+
+
+
