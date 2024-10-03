2457ede2583caf54631cbb2235931b862781995c
ilialobjanidze <lobjanidzeilia10@gmail.com>
2024-10-03 18:26:17 +0400

    twentyfourth commit

---
+++ level 34/classwork/homework.py
@@ -0,0 +1,6 @@
+#SyntaxError == Incorrect code structure or syntax.
+#NameError ==  Undefined or unrecognized variable.
+#TypeError == Wrong operation between different data types.
+#IndexError ==  List or sequence index is out of range.
+
+#We use Try/Except So when we occure Error Code wont stop executing
