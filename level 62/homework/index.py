00c8cdaff1c2b0fbeb1c0b6ec58a5df7ea094219
ilialobjanidze <lobjanidzeilia10@gmail.com>
2024-12-24 02:21:42 +0400

    thirty-fourth commit

---
+++ level 62/homework/index.py
@@ -0,0 +1,48 @@
+#1 
+#Both indicates a "protected" attribute or method.
+#__ Use it for attributes or methods that should not be easily overridden or accessed directly,
+#_ Use it when you want to signal to other developers that the attribute is not part of the public API
+
+
+#2 
+class Level1:
+    def __init__(self):
+        self._hidden = "Level 1: Protected"
+
+    def get_hidden(self):
+        return self._hidden
+
+class Level2:
+    def __init__(self):
+        self.__hidden = "Level 2: Private"
+
+    def get_hidden(self):
+        return self.__hidden
+
+#3
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def move(self):
+        return f"{self.name} is moving."
+
+# Intermediate Class (Prey)
+class Prey(Animal):
+    def flee(self):
+        return f"{self.name} is fleeing to avoid predators."
+
+# Intermediate Class (Predator)
+class Predator(Animal):
+    def hunt(self):
+        return f"{self.name} is hunting prey."
+
+# Derived Class (Rabbit)
+class Rabbit(Prey):
+    def eat(self):
+        return f"{self.name} is eating plants."
+
+# Derived Class (Hawk)
+class Hawk(Predator):
+    def soar(self):
+        return f"{self.name} is soaring in the sky."
